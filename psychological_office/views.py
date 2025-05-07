6088b7ff747ec9c0f4d7e08bca9211eb886ea90e
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-07 15:14:07 -0300

    Create the views

--- psychological_office/views.py
+++ psychological_office/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+#------------------------ VIEWS ------------------------#
+
+from rest_framework import viewsets
+from .models import Patient, Appointment, Consultation
+from .serializers import PatientSerializer, AppointmentSerializer, ConsultationSerializer
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class ConsultationViewSet(viewsets.ModelViewSet):
+    queryset = Consultation.objects.all()
+    serializer_class = ConsultationSerializer
