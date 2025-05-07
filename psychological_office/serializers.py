e4d595febde863972d0b32c003877770fa8ae872
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-07 15:13:54 -0300

    Add a comment

--- psychological_office/serializers.py
+++ psychological_office/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Patient, Appointment, Consultation
 
+#------------------------ SERIALIZERS ------------------------#
+
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
