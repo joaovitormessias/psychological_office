2f897e7549470a827cb0a98172fae80f76808bab
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-07 14:32:36 -0300

    Create admin panel

--- psychological_office/admin.py
+++ psychological_office/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Patient, User, Appointment, Consultation
+
+#------------------------ ADMIN PANEL ------------------------#
+
+
+#Register Patient model
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('cpf', 'patient_name', 'dob')
+
+#Register User model
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'role', 'email')
+
+#Register Appointment model
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'date', 'time', 'status')
+
+#Register Consultation model
+@admin.register(Consultation)
+class ConsultationAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'date', 'status')
