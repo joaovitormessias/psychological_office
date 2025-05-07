ddecb078dd8c308945947bff304d08f05c6a6e98
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-07 15:16:35 -0300

    Define urls for the api endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,23 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework.routers import DefaultRouter
+from psychological_office import views
+
+
+router  = DefaultRouter()
+router.register(r'patients', views.PatientViewSet)
+router.register(r'appointments', views.AppointmentViewSet)
+router.register(r'consultations', views.ConsultationViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+
+    path('api/', include(router.urls)),
+]   
