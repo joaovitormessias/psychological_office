dae6cbfeff2c66f9b7ba708f94c2782812edc383
joaovitormessias <joaovitormessiasdamasio.com>
2025-05-08 09:39:38 -0300

    Generate schema URL

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from psychological_office import views
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 router  = DefaultRouter()
 router.register(r'patients', views.PatientViewSet)
@@ -18,6 +20,9 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
     path('api/', include(router.urls)),
 ]   
